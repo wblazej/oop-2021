9dfc7dcc138d95facdb46d65e2daed5938555bb4
Piotr Marecki <pmarecki.pro@gmail.com>
2021-10-22 19:48:31 +0200

    x

--- src/p1/pc_hardware/a.py
+++ src/p1/pc_hardware/a.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import List
 
 from p1.pc_hardware.cpu import CPU, AMD_CPU
@@ -9,13 +10,23 @@ class PC:
     pass
 
 
+class CpuSocket(Enum):
+    AM4 = 1
+    TR4 = 2
+    sTRX4 = 3
+    Socket_1200 = 4
+
+
 class Motherboard:
     cpu: CPU
+    cpu_socket: CpuSocket
     pcie: List[PCIeSlot] = [None] * 3
 
     def __init__(self, cpu: CPU):
         print(f'tworzę MB z CPU: {cpu} typu {type(cpu)}')
         self.cpu = cpu
+        self.cpu_socket = CpuSocket.Socket_1200
+
 
 
 class AMD_Motherboard(Motherboard):
