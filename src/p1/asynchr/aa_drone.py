80f86507fbf165134e8867786b28b034ac559e52
wblazej <blazej.wrzosok@gmail.com>
2022-01-29 10:38:31 +0100

    async D task

--- src/p1/asynchr/aa_drone.py
+++ src/p1/asynchr/aa_drone.py
@@ -1,9 +1,10 @@
 import asyncio
+from math import radians
 import threading
 from asyncio import sleep, current_task
 from dataclasses import dataclass
 from datetime import datetime
-from random import random
+from random import random, randint
 
 
 def ts():
@@ -27,6 +28,7 @@ def log(msg):
 class Pos:
     x: float
     y: float
+    z: float = random() * 100
 
 
 async def update_gps(d: Pos):
@@ -37,11 +39,16 @@ async def update_gps(d: Pos):
         d.y = random()
         await sleep(0.5)
 
+async def update_height(d: Pos):
+    while True:
+        d.z += random() * 5 * -1 if randint(0, 1) == 0 else 1
+        d.z = max(0, d.z)
+        await sleep(0.5)
 
 async def stabilize_drone(d: Pos):
     log('drone stabilization initiated')
     while True:
-        log(f'stabilizing... at: x={d.x},{d.y}')
+        log(f'stabilizing... at: x={d.x},{d.y},{d.z}')
         await sleep(0.01)
 
 
@@ -51,8 +58,9 @@ async def main_foo():
     d = Pos(0,0)
     asyncio.create_task(update_gps(d))
     asyncio.create_task(stabilize_drone(d))
+    asyncio.create_task(update_height(d))
     log(f'main -- done after {ts() - st:.3f}s')
-    await sleep(2)
+    await sleep(10)
 
 
 asyncio.run(main_foo())
