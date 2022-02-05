852f8f014df3df59bedbfdb6b6429c15594cbc53
Piotr Marecki <pmarecki.pro@gmail.com>
2022-02-05 10:07:52 +0100

    monitor -- entry point

--- src/p1/asynchr/simple_server.py
+++ src/p1/asynchr/simple_server.py
@@ -1,7 +1,8 @@
 from asyncio import sleep
 
 from aiohttp import web
-from os import uname
+# from os import uname
+from p1.asynchr.toolz import is_really_prime
 
 routes = web.RouteTableDef()
 
@@ -12,15 +13,24 @@ query = req.rel_url.query['query']  # params; required; else .get('query','defau
 """
 
 
+
+
+
+
 @routes.get('/')
-async def hello(request):
-    n = uname()
-    print(f'{n.nodename} request received')
-    return web.json_response({'comment': f'hello from {n.nodename}!'})
+async def hello1(request):
+    n = 'kadabra'
+    return web.json_response({'comment': f'hello from n!'})
+
+
+# @routes.get('/start_monitor')
+# @routes.get('/monitor_results') # --> wyślij zdjęcie do klienta...
+
+
 
 
 @routes.get('/welcome')
-async def hello(request):
+async def hello2(request):
     name = request.rel_url.query['name']
     await sleep(1.2)
     print(f'welcome request received for {name}')
@@ -28,10 +38,16 @@ async def hello(request):
 
 
 @routes.get('/add')
-async def hello(request):
+async def hello3(request):
     a = float(request.rel_url.query['a'])
     b = float(request.rel_url.query['b'])
     return web.json_response({'result': a + b})
+
+@routes.get('/is_prime')
+async def hello4(request):
+    x = int(request.rel_url.query['x'])
+    res = await is_really_prime(x)
+    return web.json_response({'result': res})
 
 
 app = web.Application()
