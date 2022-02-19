28aa3ca89f585e4b374afbd3d8087cb01d2d6f79
wblazej <blazej.wrzosok@gmail.com>
2022-02-19 11:41:02 +0100

    aiohttp serve files

--- src/p1/asynchr/new_uploader_serwer.py
+++ src/p1/asynchr/new_uploader_serwer.py
@@ -1,11 +1,12 @@
-import os.path
 from asyncio import sleep
 from random import randint
+from urllib import request
 
 from aiohttp import web
 from aiohttp.abc import BaseRequest
-from aiohttp.web_middlewares import middleware
 from faker import Faker
+import os
+from os import path
 
 routes = web.RouteTableDef()
 
@@ -16,27 +17,44 @@ query = req.match_info.get('query', '')  # for route-resolving, /{query}
 query = req.rel_url.query['query']  # params; required; else .get('query','default')
 """
 
+def is_safe_path(basedir, path):
+    return basedir == os.path.commonpath((basedir, os.path.abspath(path)))
+
 
 @routes.get('/')
 async def hello(request):
     print('request received')
     return web.json_response({'comment': f'hello, x={12}!'})
 
-@middleware
-async def middleware(request, handler):
-    # "opakowuje" każdy request... można tu zrobić try... expect...
-    print(f'request: {request}')
-    resp = await handler(request)
-    print(f'response: {resp.status}')
-    return resp
 
-@routes.get('/serve')
-async def serve_file(request):
-    filename = request.rel_url.query.get('filename', '')
-    path = f'images/{filename}'
-    if '..' in filename or '/' in filename or "\\" in filename or not os.path.isfile(path):
-        raise RuntimeError('Invalid filename')
-    return web.FileResponse(f'images/{filename}')
+@routes.get('/welcome')
+async def welcome(request):
+    name = request.rel_url.query['name']
+    await sleep(1.2)
+    print(f'welcome request received for {name}')
+    return web.json_response({'comment': f'hello {name}!'})
+
+
+@routes.get('/users/{userid}/details')
+async def welcome(request):
+    # http://0.0.0.0:8888/users/i8811/details
+    userid = request.match_info.get('userid', '')
+    fake = Faker()
+    user_name = fake.name()
+    user_address = fake.address().replace('\n',', ')
+    resp = {'userid': userid, 'name': user_name, 'address': user_address}
+    return web.json_response(resp)
+
+
+@routes.get('/serve/{filename}')
+async def serve_file(req):
+    filename = req.match_info.get('filename', '')
+    if is_safe_path(os.getcwd(), filename):
+        file_path = f'images/{filename}'
+        if not path.exists(file_path):
+            return web.HTTPNotFound()
+        return web.FileResponse(file_path)
+    return web.HTTPBadRequest()
 
 @routes.post('/upload')
 async def accept_file(req: BaseRequest):
@@ -84,6 +102,6 @@ async def starter():
     return app
 
 
-app = web.Application(middlewares=[middleware])
+app = web.Application()
 app.add_routes(routes)
 web.run_app(starter(), port=8888)  # ewentu
